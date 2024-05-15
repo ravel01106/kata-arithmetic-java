809962e861d05df32cc7cdbb23a2eb0e37c9179c
ravel01106 <amandaravelocabrera@gmail.com>
2024-05-15 12:30:33 +0100

    Refactor: eliminated import unnecessary

--- src/test/java/org/arithmetic/ArithmeticShould.java
+++ src/test/java/org/arithmetic/ArithmeticShould.java
@@ -1,8 +1,6 @@
 package org.arithmetic;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class ArithmeticShould {
